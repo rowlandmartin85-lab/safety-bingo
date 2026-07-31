48dc5e650f026a1d674b346e18ba66646790aa97
rowlandmartin85-lab <rowlandmartin85@gmail.com>
2026-07-31 09:46:26 -0400

    server.js

--- server.js
+++ server.js
@@ -1498,16 +1498,22 @@ const PORT =
 process.env.PORT || 3000;
 
 
-
-server.listen(
-PORT,
-"0.0.0.0",
-()=>{
+loadQuestionsFromDatabase()
+.then(()=>{
 
 
-console.log(
-`Safety Bingo running on port ${PORT}`
-);
+    server.listen(
+    PORT,
+    "0.0.0.0",
+    ()=>{
+
+
+        console.log(
+        `Safety Bingo running on port ${PORT}`
+        );
+
+
+    });
 
 
 });
