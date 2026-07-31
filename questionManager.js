402e101b2b7582a7451d8111d3a67c09481a4154
rowlandmartin85-lab <rowlandmartin85@gmail.com>
2026-07-30 20:35:55 -0400

    questionManager.js

---
+++ questionManager.js
@@ -0,0 +1,39 @@
+console.log(
+"QUESTION MANAGER LOADED"
+);
+
+
+const questionInput =
+document.getElementById(
+"newQuestion"
+);
+
+
+const answerInput =
+document.getElementById(
+"newAnswer"
+);
+
+
+const addBtn =
+document.getElementById(
+"addQuestionBtn"
+);
+
+
+const list =
+document.getElementById(
+"questionList"
+);
+
+
+const removeBtn =
+document.getElementById(
+"removeQuestionBtn"
+);
+
+
+
+console.log(
+"Question Manager Ready"
+);
